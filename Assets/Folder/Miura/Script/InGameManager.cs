497352d61e98cc3f432864cbd59984d338d56e1f
KuronekoCoffee <bccooffee@gamail.com>
2025-11-15 18:01:05 +0900

    エラーを直した。

--- Assets/Folder/Miura/Script/InGameManager.cs
+++ Assets/Folder/Miura/Script/InGameManager.cs
@@ -5,11 +5,13 @@ public class InGameManager : MonoBehaviour
     [Header("制限時間")]
     [SerializeField] float _limitTime;
     private float _timer = 0f;
-    
-    [SerializeField] private Container _container;
+
+    [SerializeField] private GameObject _gameOverUI;
+
     void Update()
     {
         _timer += Time.deltaTime;
+
         if (_timer > _limitTime)
         {
             GameOver();
@@ -18,8 +20,16 @@ public class InGameManager : MonoBehaviour
 
     void GameOver()
     {
-        _container.gameObject.SetActive(true);
-        _container._gameOverUI.GetComponent<Animator>().Play("ActiveGameOver");
-        
+        _gameOverUI.SetActive(true);
+
+        Animator anim = _gameOverUI.GetComponent<Animator>();
+        if (anim != null)
+        {
+            anim.Play("ActiveGameOver");
+        }
+        else
+        {
+            Debug.LogWarning("GameOverUI に Animator がついてません！");
+        }
     }
 }
