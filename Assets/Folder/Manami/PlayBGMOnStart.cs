0a01bf9b10d0994e061d25a7d82bd7a76b55530a
hirayama0216 <caren.manami@gmail.com>
2025-11-15 23:36:40 +0900

    BGMがなるゲームオブジェクト追加

---
+++ Assets/Folder/Manami/PlayBGMOnStart.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using SoundSystem;
+
+public class PlayBGMOnStart:
+MonoBehaviour
+{
+    [SerializeField]
+    private string
+    bgmName = "GameBGM";
+
+    void Start()
+    {
+        SoundManager.Instance.PlayBGMByName(bgmName);
+    }
+}
