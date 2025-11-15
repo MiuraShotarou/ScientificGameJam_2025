50029759d212d4e918be957a0346f15f16451547
seriren <seriren0921@gmail.com>
2025-11-15 13:20:16 +0900

    プレイヤーの横移動、ジャンプ、さる１，２追加

---
+++ Assets/Script/Monkey/FollowPlayerTest.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class FollowPlayerTest : MonoBehaviour
+{
+    public Transform target;  // プレイヤー
+    public float followSpeed = 5f;
+    public Vector3 offset = new Vector3(-1f, 0f, 0f);
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(
+            transform.position,
+            target.position + offset,
+            followSpeed * Time.deltaTime
+        );
+    }
+}
