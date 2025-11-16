53b3d7a1a0f04bf4504c68b78d99a469f1800628
Awa <tk220171@tks.iput.ac.jp>
2025-11-16 10:07:38 +0900

    BGMをSoundManagerにセット

---
+++ Assets/Folder/Awamura/PlayBGMOnStart.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using SoundSystem;   // ← これが必要だったはず
+
+public class PlayBGMOnStart : MonoBehaviour
+{
+    [SerializeField] private string bgmName = "GameBGM";
+
+    void Start()
+    {
+        SoundManager.Instance.PlayBGMByName(bgmName);
+    }
+}
---
+++ Assets/Sounds/SoundSystem/EnvDatabase1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace SoundSystem
+{
+    [System.Serializable]
+    public class EnvironmentEntry
+    {
+        public string envName;
+        public AudioClip audioClip;
+        [Range(0f, 2f)] public float volumeMultiplier = 1f;
+    }
+
+    [CreateAssetMenu(menuName = "Sound/Environment Database")]
+    public class EnvDatabase : ScriptableObject
+    {
+        public List<EnvironmentEntry> envList;
+
+        public EnvironmentEntry GetEntryByName(string envName)
+        {
+            return envList.FirstOrDefault(e => e.envName == envName);
+        }
+
+        public AudioClip GetClipByName(string envName)
+        {
+            return GetEntryByName(envName)?.audioClip;
+        }
+    }
+}
